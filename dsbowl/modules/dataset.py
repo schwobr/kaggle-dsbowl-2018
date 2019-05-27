144690b167e8f55be78afe4d8256fa29c97e35d2
Robin Schwob <robin.schwob@student.ecp.fr>
2019-05-27 14:57:44 +0200

    debug

--- dsbowl/modules/dataset.py
+++ dsbowl/modules/dataset.py
@@ -317,7 +317,7 @@ class MultiMasksList(SegmentationLabelList):
                     np.ones((3, 3),
                             np.uint8),
                     iterations=1)).unsqueeze(0)
-        return ImageSegment(mask).float()
+        return ImageSegment(mask.float())
 
     def analyze_pred(self, pred, thresh: float = 0.5):
         return (pred > thresh).float()

249c426e1b74f494561cfd1bf441ea0503b90664
Robin Schwob <robin.schwob@student.ecp.fr>
2019-05-28 14:07:58 +0200

    debug

--- dsbowl/config.py
+++ dsbowl/config.py
@@ -13,7 +13,7 @@ PROJECT_PATH = '/work/stages/schwob/data-science-bowl-2018/kaggle-dsbowl-2018/'
 TRAIN_PATH = PROJECT_PATH+'data/stage1_train/'
 TEST_PATH = PROJECT_PATH+'data/stage2_test_final/'
 MODELS_PATH = PROJECT_PATH+'models/'
-SUB_PATH = 'submissions/'
+SUB_PATH = PROJECT_PATH+'submissions/'
 
 # NORMALIZE
 MEAN = (0.5, 0.5, 0.5)

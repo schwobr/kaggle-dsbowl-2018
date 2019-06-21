785e68d409f23db2cadde83485e266fb7e2a0624
Robin Schwob <robin.schwob@student.ecp.fr>
2019-06-21 13:31:05 +0200

    updated config

--- dsbowl/config.py
+++ dsbowl/config.py
@@ -1,19 +1,22 @@
+from pathlib import Path
+
 # IMAGE SIZES
-TRAIN_WIDTH = 256
-TRAIN_HEIGHT = 256
-MAX_WIDTH = 1388
-MAX_HEIGHT = 1388
-TEST_HEIGHT = 256
-TEST_WIDTH = 256
+TRAIN_SIZE = 256
+MAX_SIZE = 1388
+TEST_SIZE = 224
 TEST_OVERLAP = 64
 IMG_CHANNELS = 3
 
 # PATHS
-PROJECT_PATH = '/work/stages/schwob/data-science-bowl-2018/kaggle-dsbowl-2018/'
-TRAIN_PATH = PROJECT_PATH+'data/stage1_train/'
-TEST_PATH = PROJECT_PATH+'data/stage2_test_final/'
-MODELS_PATH = PROJECT_PATH+'models/'
-SUB_PATH = PROJECT_PATH+'submissions/'
+PROJECT_PATH = Path(
+    '/work/stages/schwob/data-science-bowl-2018/kaggle-dsbowl-2018/')
+TRAIN_PATH = PROJECT_PATH/'data/stage1_train/'
+TEST_PATH = PROJECT_PATH/'data/stage2_test_final/'
+MODELS_PATH = PROJECT_PATH/'models/'
+SUB_PATH = PROJECT_PATH/'submissions/'
+TRAIN_CSV = PROJECT_PATH/'stage1_train.csv'
+TEST_CSV = PROJECT_PATH/'stage2_test_final.csv'
+LOG = Path('/work/stages/schwob/runs')
 
 # NORMALIZE
 MEAN = (0.5, 0.5, 0.5)
@@ -22,6 +25,12 @@ STD = (0.5, 0.5, 0.5)
 # LEARNER CONFIG
 BATCH_SIZE = 4
 WD = 0.1
-LR = 2e-4
-EPOCHS = 100
-MODEL = "resnet34"
+LRS = [2e-4]
+GROUP_LIMITS = None
+FREEZE_UNTIL = None
+EPOCHS = 10
+UNFROZE_EPOCHS = None
+PRETRAINED = False
+MODEL = 'resnet34'
+CLASSES = 1
+ACT = 'sigmoid'
